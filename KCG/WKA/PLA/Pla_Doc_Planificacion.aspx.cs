17aa95f7f05ebcf411dc8f2d5842580b70ed91d6
vmejiaec <vmejiaec@gmail.com>
2014-10-20 05:02:11 +0000

    Antes de empezar con los reportes

--- KCG/WKA/PLA/Pla_Doc_Planificacion.aspx.cs
+++ KCG/WKA/PLA/Pla_Doc_Planificacion.aspx.cs
@@ -188,34 +188,44 @@ public partial class PLA_Pla_Doc_Planificacion : PaginaBase
                                      "Codigo: " + (string)e.Values["Codigo"];
     }
     protected void fvPla_Doc_ItemUpdating(object sender, FormViewUpdateEventArgs e)
-    {
-        // Controla el cambio del formato de las fechas
-        e.NewValues["Fecha_Solicita"] = DateTime.Parse((string)e.NewValues["Fecha_Solicita"]);
-        e.OldValues["Fecha_Solicita"] = DateTime.Parse((string)e.OldValues["Fecha_Solicita"]);
-        e.NewValues["Fecha_Contrata"] = DateTime.Parse((string)e.NewValues["Fecha_Contrata"]);
-        e.OldValues["Fecha_Contrata"] = DateTime.Parse((string)e.OldValues["Fecha_Contrata"]);
-        e.NewValues["Fecha_Planifica"] = DateTime.Parse((string)e.NewValues["Fecha_Planifica"]);
-        e.OldValues["Fecha_Planifica"] = DateTime.Parse((string)e.OldValues["Fecha_Planifica"]);
-        // Valores para los campos de personas
-        e.NewValues["Per_Personal_Id_Modifica"] = Scope.Per_Personal_Id;
-        // La solicitud del director se actualiza con las personas de Contratación
-        // en NULL para que sea coherente con el proceso
-        if (String.IsNullOrEmpty((string)e.OldValues["Per_Personal_Id_Contrata"]))
+    {// Puede editar siempre y cuando el Esta_Contratada sea PEN
+        string sEsta_Contratada = (string) e.OldValues["Esta_Contratada"];
+        if (sEsta_Contratada == "PEN")
         {
-            e.OldValues["Per_Personal_Id_Contrata"] = null;
-            e.NewValues["Per_Personal_Id_Contrata"] = null;
-        }
-        if (String.IsNullOrEmpty((string)e.OldValues["Per_Personal_Id_Planifica"]))
-        {
-            e.OldValues["Per_Personal_Id_Planifica"] = null;
-        }
-        // Cambio del formato del campo Valor
-        e.NewValues["Valor_Solicita"] = Decimal.Parse((string)e.NewValues["Valor_Solicita"]);
-        e.OldValues["Valor_Solicita"] = Decimal.Parse((string)e.OldValues["Valor_Solicita"]);
+            // Controla el cambio del formato de las fechas
+            e.NewValues["Fecha_Solicita"] = DateTime.Parse((string)e.NewValues["Fecha_Solicita"]);
+            e.OldValues["Fecha_Solicita"] = DateTime.Parse((string)e.OldValues["Fecha_Solicita"]);
+            e.NewValues["Fecha_Contrata"] = DateTime.Parse((string)e.NewValues["Fecha_Contrata"]);
+            e.OldValues["Fecha_Contrata"] = DateTime.Parse((string)e.OldValues["Fecha_Contrata"]);
+            e.NewValues["Fecha_Planifica"] = DateTime.Parse((string)e.NewValues["Fecha_Planifica"]);
+            e.OldValues["Fecha_Planifica"] = DateTime.Parse((string)e.OldValues["Fecha_Planifica"]);
+            // Valores para los campos de personas
+            e.NewValues["Per_Personal_Id_Modifica"] = Scope.Per_Personal_Id;
+            // La solicitud del director se actualiza con las personas de Contratación
+            // en NULL para que sea coherente con el proceso
+            if (String.IsNullOrEmpty((string)e.OldValues["Per_Personal_Id_Contrata"]))
+            {
+                e.OldValues["Per_Personal_Id_Contrata"] = null;
+                e.NewValues["Per_Personal_Id_Contrata"] = null;
+            }
+            if (String.IsNullOrEmpty((string)e.OldValues["Per_Personal_Id_Planifica"]))
+            {
+                e.OldValues["Per_Personal_Id_Planifica"] = null;
+            }
+            // Cambio del formato del campo Valor
+            e.NewValues["Valor_Solicita"] = Decimal.Parse((string)e.NewValues["Valor_Solicita"]);
+            e.OldValues["Valor_Solicita"] = Decimal.Parse((string)e.OldValues["Valor_Solicita"]);
 
-        // Guarda los datos del registro a borrar en memoria
-        this.MemoriaRegistroActual = "Id: " + (string)e.NewValues["Id"] + " * " +
-                                     "Codigo: " + (string)e.NewValues["Codigo"];
+            // Guarda los datos del registro a borrar en memoria
+            this.MemoriaRegistroActual = "Id: " + (string)e.NewValues["Id"] + " * " +
+                                         "Codigo: " + (string)e.NewValues["Codigo"];
+        }
+        else // Si no se cumple que Esta_Contratada sea PEN
+        {
+            e.Cancel = true;
+            fvPla_Doc.HayErrorInsUpd = true;
+            lbFvMsgErrorPla_Doc.Text = String.Format("Error, el campo Esta_Contratada debe estar en PEN.");
+        }
     }
     protected void fvPla_Doc_ItemDeleting(object sender, FormViewDeleteEventArgs e)
     {
@@ -398,73 +408,139 @@ public partial class PLA_Pla_Doc_Planificacion : PaginaBase
     }
     protected void fvPla_Mov_ItemInserting(object sender, FormViewInsertEventArgs e)
     {
-        // Valor por defecto del Id y Orden
-        e.Values["Id"] = -1;
-        if (String.IsNullOrWhiteSpace((string)e.Values["Estado"])) e.Values["Estado"] = "PEN";
-        e.Values["Orden"] = 0;
-        e.Values["Pla_Partida_Id"] = 0;
-        // Cambio del formato de los campos de Valor para asegurarse que siempre sea negativo
-        e.Values["Valor"] = Decimal.Parse((string)e.Values["Valor"]);
-        // Pone el Tipo del Movimiento en DEB
-        e.Values["Tipo"] = "DEB";
-        // VALIDA si el valor ingresado supera el saldo permitido
-        string sValor_Suma = ((TextBox) fvPla_Mov.FindControl("Valor_SumaTextBox")).Text;
-        Decimal dValor_Suma = Decimal.Parse(sValor_Suma);
-        Decimal dValor_f = (Decimal)e.Values["Valor"];
-        if (dValor_f > dValor_Suma)
+        //Valida que el documento tenga Esta_Contrata = PEN
+        var xEsta_ContratadaTextBox = fvPla_Doc.FindControl("Esta_ContratadaTextBox");
+        if (xEsta_ContratadaTextBox != null)
+        {
+            // 
+            string sEsta_Contratada = ((TextBox)xEsta_ContratadaTextBox).Text;
+            if (sEsta_Contratada == "PEN")
+            {
+                // Valor por defecto del Id y Orden
+                e.Values["Id"] = -1;
+                if (String.IsNullOrWhiteSpace((string)e.Values["Estado"])) e.Values["Estado"] = "PEN";
+                e.Values["Orden"] = 0;
+                e.Values["Pla_Partida_Id"] = 0;
+                // Cambio del formato de los campos de Valor para asegurarse que siempre sea negativo
+                e.Values["Valor"] = Decimal.Parse((string)e.Values["Valor"]);
+                // Pone el Tipo del Movimiento en DEB
+                e.Values["Tipo"] = "DEB";
+                // VALIDA si el valor ingresado supera el saldo permitido
+                string sValor_Suma = ((TextBox)fvPla_Mov.FindControl("Valor_SumaTextBox")).Text;
+                Decimal dValor_Suma = Decimal.Parse(sValor_Suma);
+                Decimal dValor_f = (Decimal)e.Values["Valor"];
+                if (dValor_f > dValor_Suma)
+                {
+                    e.Cancel = true;
+                    fvPla_Mov.HayErrorInsUpd = true;
+                    lbFvMsgErrorPla_Mov.Text = String.Format("El valor {0:N2} no puede superar al saldo.", dValor_f);
+                }
+                else
+                {
+                    lbFvMsgErrorPla_Mov.Text = "";
+                }
+                // Guarda los datos del registro a borrar en memoria
+                this.MemoriaRegistroActual = "Codigo: " + (string)e.Values["Codigo"];
+            }
+            else // Si el Estado_Contrata es diferente de PEN
+            {
+                e.Cancel = true;
+                fvPla_Mov.HayErrorInsUpd = true;
+                lbFvMsgErrorPla_Mov.Text = "Error, la solicitud debe tener el Estado_Contrata en PEN.";
+            }
+        }
+        else // En caso que no haya seleccionado nada en la grilla de DOC
         {
             e.Cancel = true;
             fvPla_Mov.HayErrorInsUpd = true;
-            lbFvMsgErrorPla_Mov.Text = String.Format("El valor {0:N2} no puede superar al saldo.", dValor_f);
+            lbFvMsgErrorPla_Mov.Text = "Primero debe seleccionar una solicitud de la lista.";
         }
-        else
-        {
-            lbFvMsgErrorPla_Mov.Text = "";
-        }
-        // Guarda los datos del registro a borrar en memoria
-        this.MemoriaRegistroActual = "Codigo: " + (string)e.Values["Codigo"];
     }
     protected void fvPla_Mov_ItemUpdating(object sender, FormViewUpdateEventArgs e)
     {
-        // Controla el cambio del formato de las fechas
-        e.NewValues["Pla_Doc_Fecha"] = DateTime.Parse((string)e.NewValues["Pla_Doc_Fecha"]);
-        e.OldValues["Pla_Doc_Fecha"] = DateTime.Parse((string)e.OldValues["Pla_Doc_Fecha"]);
-        // Cambio del formato de los campos de Valor 
-        e.NewValues["Valor"] = Decimal.Parse((string)e.NewValues["Valor"]);
-        e.OldValues["Valor"] = Decimal.Parse((string)e.OldValues["Valor"]);
-        // Pone el Tipo del Movimiento en DEB
-        e.NewValues["Tipo"] = "DEB";
-        e.OldValues["Tipo"] = "DEB";
-        // VALIDA si el valor ingresado supera el saldo permitido
-        string sValor_Suma = ((TextBox)fvPla_Mov.FindControl("Valor_SumaTextBox")).Text;
-        Decimal dValor_Suma = Decimal.Parse(sValor_Suma);
-        Decimal dValor_o = (Decimal) e.OldValues["Valor"];
-        Decimal dValor_f = (Decimal)e.NewValues["Valor"];
-        if (dValor_f > dValor_o + dValor_Suma )
+        // Valida si ha seleccionado una fila en DOC
+        var xEsta_ContratadaTextBox = fvPla_Doc.FindControl("Esta_ContratadaTextBox");
+        if (xEsta_ContratadaTextBox != null)
+        {
+            // Valida que el documento tenga Esta_Contrata = PEN
+            string sEsta_Contratada = ((TextBox)xEsta_ContratadaTextBox).Text;
+            if (sEsta_Contratada == "PEN")
+            {
+                // Controla el cambio del formato de las fechas
+                e.NewValues["Pla_Doc_Fecha"] = DateTime.Parse((string)e.NewValues["Pla_Doc_Fecha"]);
+                e.OldValues["Pla_Doc_Fecha"] = DateTime.Parse((string)e.OldValues["Pla_Doc_Fecha"]);
+                // Cambio del formato de los campos de Valor 
+                e.NewValues["Valor"] = Decimal.Parse((string)e.NewValues["Valor"]);
+                e.OldValues["Valor"] = Decimal.Parse((string)e.OldValues["Valor"]);
+                // Pone el Tipo del Movimiento en DEB
+                e.NewValues["Tipo"] = "DEB";
+                e.OldValues["Tipo"] = "DEB";
+                // VALIDA si el valor ingresado supera el saldo permitido
+                string sValor_Suma = ((TextBox)fvPla_Mov.FindControl("Valor_SumaTextBox")).Text;
+                Decimal dValor_Suma = Decimal.Parse(sValor_Suma);
+                Decimal dValor_o = (Decimal)e.OldValues["Valor"];
+                Decimal dValor_f = (Decimal)e.NewValues["Valor"];
+                if (dValor_f > dValor_o + dValor_Suma)
+                {
+                    e.Cancel = true;
+                    fvPla_Mov.HayErrorInsUpd = true;
+                    lbFvMsgErrorPla_Mov.Text = String.Format("El valor de {0:N2} causará un saldo negativo.", dValor_f);
+                }
+                else
+                {
+                    lbFvMsgErrorPla_Mov.Text = "";
+                }
+                // Guarda los datos del registro a borrar en memoria
+                this.MemoriaRegistroActual = "Id: " + (string)e.NewValues["Id"] + " * " +
+                                             "Codigo: " + (string)e.NewValues["Codigo"];
+            }
+            else // Si el Estado_Contrata es diferente de PEN
+            {
+                e.Cancel = true;
+                fvPla_Mov.HayErrorInsUpd = true;
+                lbFvMsgErrorPla_Mov.Text = "Error, la solicitud debe tener el Estado_Contrata en PEN.";
+            }
+        }
+        else // En caso que no haya seleccionado nada en la grilla de DOC
         {
             e.Cancel = true;
             fvPla_Mov.HayErrorInsUpd = true;
-            lbFvMsgErrorPla_Mov.Text = String.Format( "El valor de {0:N2} causará un saldo negativo.", dValor_f);
+            lbFvMsgErrorPla_Mov.Text = "Primero debe seleccionar una solicitud de la lista.";
         }
-        else
-        {
-            lbFvMsgErrorPla_Mov.Text = "";
-        }
-        // Guarda los datos del registro a borrar en memoria
-        this.MemoriaRegistroActual = "Id: " + (string)e.NewValues["Id"] + " * " +
-                                     "Codigo: " + (string)e.NewValues["Codigo"];
     }
     protected void fvPla_Mov_ItemDeleting(object sender, FormViewDeleteEventArgs e)
     {
-        // Control de valores antes del borrado de fechas 
-        e.Values["Pla_Doc_Fecha"] = DateTime.Parse((string)e.Values["Pla_Doc_Fecha"]);
-        // Valores
-        e.Values["Valor"] = Decimal.Parse((string)e.Values["Valor"]);
-        // Pone el Tipo del Movimiento en DEB
-        e.Values["Tipo"] = "DEB";
-        // Guarda los datos del registro a borrar en memoria
-        this.MemoriaRegistroActual = "Id: " + (string)e.Values["Id"] + " * " +
-                                     "Codigo: " + (string)e.Values["Codigo"];
+        // Valida si ha seleccionado una fila en DOC
+        var xEsta_ContratadaTextBox = fvPla_Doc.FindControl("Esta_ContratadaTextBox");
+        if (xEsta_ContratadaTextBox != null)
+        {
+            // Valida que el documento tenga Esta_Contrata = PEN
+            string sEsta_Contratada = ((TextBox)xEsta_ContratadaTextBox).Text;
+            if (sEsta_Contratada == "PEN")
+            {
+                // Control de valores antes del borrado de fechas 
+                e.Values["Pla_Doc_Fecha"] = DateTime.Parse((string)e.Values["Pla_Doc_Fecha"]);
+                // Valores
+                e.Values["Valor"] = Decimal.Parse((string)e.Values["Valor"]);
+                // Pone el Tipo del Movimiento en DEB
+                e.Values["Tipo"] = "DEB";
+                // Guarda los datos del registro a borrar en memoria
+                this.MemoriaRegistroActual = "Id: " + (string)e.Values["Id"] + " * " +
+                                             "Codigo: " + (string)e.Values["Codigo"];
+            }
+            else // Si el Estado_Contrata es diferente de PEN
+            {
+                e.Cancel = true;
+                fvPla_Mov.HayErrorInsUpd = true;
+                lbFvMsgErrorPla_Mov.Text = "Error, la solicitud debe tener el Estado_Contrata en PEN.";
+            }
+        }
+        else // En caso que no haya seleccionado nada en la grilla de DOC
+        {
+            e.Cancel = true;
+            fvPla_Mov.HayErrorInsUpd = true;
+            lbFvMsgErrorPla_Mov.Text = "Primero debe seleccionar una solicitud de la lista.";
+        }
     }
     // Inicializa los valores antes de que el FormView se dibuje en la página
     protected void fvPla_Mov_PreRender(object sender, EventArgs e)
@@ -687,6 +763,5 @@ public partial class PLA_Pla_Doc_Planificacion : PaginaBase
         var oMov = (adpMov.GetById(Scope, xMovId))[0];
         o["p_Pla_Tarea_Id"] = oMov.Pla_Tarea_Id;
     }
-
 
 }
