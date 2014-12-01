ebcacf0ba6b9c177176abfee8cbfedaf9c3c8043
vmejiaec@gmail.com <vmejiaec@gmail.com@84929e83-4b22-62b8-f029-2615b69b9325>
2014-12-01 21:38:09 +0000

    Clase para convertir los numeros decimales a las letras escritas en el contrato
    EscribirEnteroALetras

--- trunk/KCG/zTest/Program.cs
+++ trunk/KCG/zTest/Program.cs
@@ -14,10 +14,13 @@ namespace zTest
 
         static void Main(string[] args)
         {
-            DocWord doc = new DocWord(@"..\..\WordTemplates\CONTRATODEEJECUCIONDEOBRACOTIZACIÓN.docx", @"..\..\CONTRATONUEVO.docx");
-            doc.ReemplazarMarcas();
-            doc.SaveDocument();
-            doc.CloseDocument();
+            //DocWord doc = new DocWord(@"..\..\WordTemplates\CONTRATODEEJECUCIONDEOBRACOTIZACIÓN.docx", @"..\..\CONTRATONUEVO.docx");
+            //doc.ReemplazarMarcas();
+            //doc.SaveDocument();
+            //doc.CloseDocument();
+            decimal numero = 900909009.09m;
+            EscribirEnteroALetras ALetras = new EscribirEnteroALetras();
+            string[] sNumero = ALetras.Convertir_Decimal(numero);
         }
     }
 }
